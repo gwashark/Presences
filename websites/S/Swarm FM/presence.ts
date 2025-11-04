9760850008ba89de2940cc32d53083d55ec185be
Haruka Ma <mrx@hcc.im>
2025-11-05 08:26:34 +0900

    read song data from dedicated element and support play status

--- websites/S/Swarm FM/presence.ts
+++ websites/S/Swarm FM/presence.ts
@@ -10,14 +10,34 @@ enum ActivityAssets {
 }
 
 presence.on('UpdateData', async () => {
-  const presenceData: PresenceData = {
-    largeImageKey: ActivityAssets.Logo,
-    startTimestamp: browsingTimestamp,
-    smallImageKey: Assets.Play,
-    state: "Listening",
-    type: ActivityType.Listening
+  let details = 'Unknown song'
+  let state = 'Unknown artist'
+  let artwork: ActivityAssets | string = ActivityAssets.Logo
+  let playing = true
+
+  // website has first-party support of PreMiD, fetch the song data from data attributes
+  const premid_data_element = document.getElementById('premid')
+  if (premid_data_element) {
+    details = premid_data_element.dataset.title ?? details
+    state = premid_data_element.dataset.artist ?? state
+    artwork = premid_data_element.dataset.artwork ?? artwork
   }
 
-  presence.setActivity(presenceData)
-})
+  const video_elements = document.getElementsByTagName('video')
+  if (video_elements.length > 0) {
+    const video_element = video_elements[0]!
+    playing = !video_element.paused
+  }
 
+  const presenceData: PresenceData = {
+    largeImageKey: artwork,
+    startTimestamp: browsingTimestamp,
+    smallImageKey: playing ? Assets.Play : Assets.Pause,
+    name: 'Swarm FM',
+    details,
+    state,
+    type: ActivityType.Listening,
+  }
+
+  await presence.setActivity(presenceData)
+})
