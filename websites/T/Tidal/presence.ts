b0edbf090eba59241d97c3a8cd99cc7b493551d0
theelgass <131011297+theelgass@users.noreply.github.com>
2026-04-15 10:02:18 +0200

    fix(Tidal): fix activity after Tidal UI update (#10662)

--- websites/T/Tidal/presence.ts
+++ websites/T/Tidal/presence.ts
@@ -1,6 +1,7 @@
-import { ActivityType, Assets } from 'premid'
+import { ActivityType, Assets, getTimestamps, timestampFromFormat } from 'premid'
 
 const LOGO_URL = 'https://cdn.rcd.gg/PreMiD/websites/T/Tidal/assets/logo.png'
+
 const presence = new Presence({ clientId: '901591802342150174' })
 
 async function getStrings() {
@@ -10,7 +11,6 @@ async function getStrings() {
       pause: 'general.paused',
       viewSong: 'general.buttonViewSong',
     },
-
   )
 }
 
@@ -18,7 +18,7 @@ let strings: Awaited<ReturnType<typeof getStrings>>
 let oldLang: string | null = null
 
 presence.on('UpdateData', async () => {
-  if (!document.querySelector('#footerPlayer'))
+  if (!document.querySelector('[data-test="track-info"]'))
     return presence.setActivity({ largeImageKey: LOGO_URL })
 
   const [newLang, timestamps, cover, buttons] = await Promise.all([
@@ -32,48 +32,47 @@ presence.on('UpdateData', async () => {
     oldLang = newLang
     strings = await getStrings()
   }
+
   const presenceData: PresenceData = {
     largeImageKey: LOGO_URL,
     type: ActivityType.Listening,
   }
+
   const songTitle = document.querySelector<HTMLAnchorElement>(
-    '[data-test=\'footer-track-title\'] > div > a',
+    '[data-test="footer-track-title"] a',
   )
+
   const currentTime = document.querySelector<HTMLElement>(
     'time[data-test="current-time"]',
   )?.textContent
-  const paused = document
-    .querySelector('div[data-test="play-controls"] div > button')
-    ?.getAttribute('data-test') === 'play'
-  const repeatType = document
-    .querySelector(
-      'div[data-test="play-controls"] > button[data-test="repeat"]',
-    )
-    ?.getAttribute('data-type')
+
+  const paused = !document.querySelector('button[data-test="pause"]')
 
   presenceData.details = songTitle?.textContent
-  // get artists
+
   presenceData.state = Array.from(
-    document.querySelectorAll<HTMLAnchorElement>('#footerPlayer .artist-link a'),
+    document.querySelectorAll<HTMLAnchorElement>(
+      '[data-test="footer-artist-name"] a',
+    ),
   )
     .map(artist => artist.textContent)
     .join(', ')
 
   if (cover) {
     presenceData.largeImageKey = document
-      .querySelector(
-        'figure[data-test=current-media-imagery] > div > div > div > img',
+      .querySelector<HTMLImageElement>(
+        'figure[data-test="current-media-imagery"] img',
       )
       ?.getAttribute('src')
       ?.replace('80x80', '640x640')
   }
-  if (
-    (Number.parseFloat(currentTime?.[0] ?? '') * 60 + Number.parseFloat(currentTime?.[1] ?? '')) * 1000 > 0
-    || !paused
-  ) {
-    [presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestamps(
-      presence.timestampFromFormat(currentTime ?? ''),
-      presence.timestampFromFormat(
+
+  const [min = 0, sec = 0] = (currentTime ?? '0:0').split(':').map(Number)
+
+  if ((min * 60 + sec) * 1000 > 0 || !paused) {
+    [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
+      timestampFromFormat(currentTime ?? ''),
+      timestampFromFormat(
         document.querySelector<HTMLElement>('time[data-test="duration"]')
           ?.textContent ?? '',
       ),
@@ -84,16 +83,14 @@ presence.on('UpdateData', async () => {
 
   if (
     document
-      .querySelector(
-        'div[data-test="play-controls"] > button[data-test="repeat"]',
-      )
+      .querySelector('button[data-test="repeat"]')
       ?.getAttribute('aria-checked') === 'true'
   ) {
-    presenceData.smallImageKey = repeatType === 'button__repeatAll' ? Assets.Repeat : Assets.RepeatOne
-    presenceData.smallImageText = repeatType === 'button__repeatAll' ? 'Playlist on loop' : 'On loop'
-
-    delete presenceData.endTimestamp
+    const isRepeatOne = !!document.querySelector('[data-test="icon--player__repeat-once"]')
+    presenceData.smallImageKey = isRepeatOne ? Assets.RepeatOne : Assets.Repeat
+    presenceData.smallImageText = isRepeatOne ? 'On loop' : 'Playlist on loop'
   }
+
   if (buttons) {
     presenceData.buttons = [
       {
@@ -102,7 +99,9 @@ presence.on('UpdateData', async () => {
       },
     ]
   }
+
   if (!timestamps)
     delete presenceData.endTimestamp
+
   presence.setActivity(presenceData)
 })
