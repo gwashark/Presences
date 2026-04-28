9ed0583f67ae02dcbd0a12d97e70945dd40f8a10
Marlon Aviz <aviz.marlon@gmail.com>
2026-04-28 06:42:40 -0300

    fix(RTL+): add support for beta.plus.rtl.de (#10702)
    * fix(RTL+): add support for beta.plus.rtl.de
    
    * refactor(RTL+): use standalone getTimestampsFromMedia

--- websites/R/RTL+/presence.ts
+++ websites/R/RTL+/presence.ts
@@ -1,4 +1,4 @@
-import { Assets } from 'premid'
+import { Assets, getTimestampsFromMedia } from 'premid'
 
 const presence = new Presence({
   clientId: '1033504954763198545',
@@ -42,7 +42,7 @@ presence.on('UpdateData', async () => {
 
       presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play
       presenceData.smallImageText = video.paused ? strings.pause : strings.play;
-      [presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestampsfromMedia(video)
+      [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(video)
       presenceData.buttons = [
         {
           label: 'View',
